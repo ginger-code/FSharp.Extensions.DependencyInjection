b74bf8a355772bc695d786aedc8bc360b8ff5041
Chris Davis <chrisdavis@gingercode.net>
2023-07-12 03:06:18 -0400

    Typings

--- src/FSharp.Extensions.DependencyInjection/IServiceCollectionExtensions.fs
+++ src/FSharp.Extensions.DependencyInjection/IServiceCollectionExtensions.fs
@@ -6,23 +6,23 @@ open Microsoft.Extensions.DependencyInjection
 [<AutoOpen>]
 module IServiceCollectionExtensions =
     let inline private addInjectedFunctions
-        (services: #IServiceCollection)
-        (injectedFunctions: (struct (Type * obj)) seq)
+        (services : #IServiceCollection)
+        (injectedFunctions : InjectedFunctionTypeAndFactory seq)
         =
         injectedFunctions
-        |> Seq.iter (fun (struct (type', instance)) -> services.AddSingleton(type', instance) |> ignore) //todo: for generics, this should be a factory, rather than an instance
+        |> Seq.iter (fun (struct (type', factory)) -> services.AddSingleton (type', factory) |> ignore) //todo: for generics, this should be a factory, rather than an instance
 
-    let inline private addAllInjectedFunctions func (services: #IServiceCollection) =
+    let inline private addAllInjectedFunctions func (services : #IServiceCollection) =
         func () |> addInjectedFunctions services
 
     type IServiceCollection with
 
         /// Add all wrapped functions marked with the InjectedFunction attribute to the service collection
-        member this.AddAllInjectedFunctions() =
+        member this.AddAllInjectedFunctions () =
             addAllInjectedFunctions Probing.findAllInjectedFunctionsByAttribute this
             this
 
         /// Add all wrapped functions marked with the InjectedFunction attribute to the service collection using the specified degree of parallelism (max parallelism if none provided)
-        member this.AddAllInjectedFunctionsParallel(?parallelism) =
+        member this.AddAllInjectedFunctionsParallel (?parallelism) =
             addAllInjectedFunctions (Probing.findAllInjectedFunctionsByAttributeParallel parallelism) this
             this
--- src/FSharp.Extensions.DependencyInjection/Probing.fs
+++ src/FSharp.Extensions.DependencyInjection/Probing.fs
@@ -5,9 +5,11 @@ open System.Reflection
 open FSharp.Collections.ParallelSeq
 open Microsoft.FSharp.Reflection
 
+type InjectedFunctionTypeAndFactory = (struct (Type * (IServiceProvider -> obj)))
+
 module Probing =
 
-    let inline private check ([<InlineIfLambda>] f: Type -> bool) msg t =
+    let inline private check ([<InlineIfLambda>] f : Type -> bool) msg t =
         if f t then
             true
         else
@@ -16,7 +18,7 @@ module Probing =
     /// Ensure that the discovered properties have the InjectedFunction flag
     let inline private filterValidProperties seqFilter props =
         /// Ensure that the discovered property is a struct-wrapped function in a single-case DU
-        let inline isOfStructWrapperUnionTypeWithSingleFunctionCase (type': Type) =
+        let inline isOfStructWrapperUnionTypeWithSingleFunctionCase (type' : Type) =
             let isStruct = check (fun t -> t.IsValueType) "Type must be a struct"
             let isDU = check FSharpType.IsUnion "Type must be a discriminated union"
 
@@ -28,7 +30,7 @@ module Probing =
 
                 if hasOneCase then
                     let fields =
-                        cases |> Array.map (fun caseField -> caseField.GetFields()) |> Array.head
+                        cases |> Array.map (fun caseField -> caseField.GetFields ()) |> Array.head
 
                     let caseHasOneField =
                         t |> check (fun _ -> fields.Length = 1) "Union case must have exactly one field"
@@ -49,7 +51,7 @@ module Probing =
             isStruct type' && isDU type' && isValidDU type'
 
         /// Ensure that the discovered property has the InjectedFunction flag
-        let inline hasInjectedFunctionAttribute (prop: PropertyInfo) =
+        let inline hasInjectedFunctionAttribute (prop : PropertyInfo) =
             prop.CustomAttributes
             // Has attribute
             |> Seq.exists (fun attr -> attr.AttributeType = typeof<InjectedFunctionAttribute>)
@@ -61,8 +63,8 @@ module Probing =
     /// Create struct tuples of the function-pointer wrapper type and its instantiated value for each property
     let inline private createTypeValuePairs seqMap props =
         /// Create a struct tuple of the function-pointer wrapper type and its instantiated value
-        let inline createTypeValuePair (prop: PropertyInfo) =
-            struct (prop.PropertyType, prop.GetMethod.Invoke(null, [||]))
+        let inline createTypeValuePair (prop : PropertyInfo) =
+            struct (prop.PropertyType, (fun _serviceProvider -> prop.GetMethod.Invoke (null, [||])))
 
         props |> seqMap createTypeValuePair
 
@@ -72,7 +74,7 @@ module Probing =
 
     /// Scan assembly for types with static properties marked with InjectedFunctionAttribute
     let inline private findInjectedFunctionsInAssembly seqCollect seqFilter assemblies =
-        let getFieldsForAssembly (assembly: Assembly) =
+        let getFieldsForAssembly (assembly : Assembly) =
             assembly.DefinedTypes
             |> Seq.collect (fun m -> m.DeclaredProperties |> (filterValidProperties seqFilter))
 
@@ -85,7 +87,7 @@ module Probing =
         >> (createTypeValuePairs seqMap)
 
     /// Scan all loaded assemblies for wrapped function pointers for dependency injection sequentially
-    let findAllInjectedFunctionsByAttribute =
+    let findAllInjectedFunctionsByAttribute : unit -> InjectedFunctionTypeAndFactory seq =
         findAllInjectedFunctionsByAttribute' Seq.collect Seq.filter Seq.map Seq.ofArray
 
     /// Scan all loaded assemblies for wrapped function pointers for dependency injection in parallel
